8da72679db78a3852890910d5663420c29216a99
_Karupa_ <mkarupaswamy1999@gmail.com>
2025-05-09 06:47:10 +0530

    NN

--- playground-2.mongodb.js
+++ playground-2.mongodb.js
@@ -1,5 +1,5 @@
 /* global use, db */
-// MongoDB Playground
+// MongoDB Playground this is
 // To disable this template go to Settings | MongoDB | Use Default Template For Playground.
 // Make sure you are connected to enable completions and to be able to run a playground.
 // Use Ctrl+Space inside a snippet or a string literal to trigger completions.
